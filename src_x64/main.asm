cdfe9328ac772290f367b371f076d0234f5a7933
wilrak0v <wilrakov@proton.me>
2026-09-09 19:44:05 +0200

    feat(src_x64/main.asm): add 'jnz' instruction

--- src_x64/main.asm
+++ src_x64/main.asm
@@ -382,6 +382,21 @@ op_jz.no_jump:
     add r12, 2
     jmp fetch
 
+op_jnz:
+    load_register rsi
+    mov eax, dword [r13 + rsi * 4]
+    cmp rax, 0
+    jz op_jnz.no_jump
+
+    load_register rsi
+    mov eax, dword [r13 + rsi * 4]
+    shl rax, 2
+    add rax, nano_code
+    mov r12, rax
+    jmp fetch
+op_jnz.no_jump:
+    add r12, 2
+    jmp fetch
 
 op_jmpi:
     ; Fetch immediate 
@@ -442,6 +457,7 @@ op_table:
     dq op_jlt
     dq op_jgt
     dq op_jz
+    dq op_jnz
     dq op_jmpi
 
 op_table_len = ($ - op_table) / 8 - 1 
@@ -475,8 +491,8 @@ halt_msg_len = $ - halt_msg
 
 ; NANO_CODE (that's just a str to jump to it easily in VIM)
 nano_code:
-    db 0x01, 1, 0, 0x00 ; movi r01, 2
+    db 0x01, 1, 190, 0x00 ; movi r01, 2
     db 0x01, 2, 2, 0x00
-    db 26, 1, 2, 0x00 ; jz r01, 1
+    db 27, 1, 2, 0x00 ; jnz r01, 1
     db 0x00, 1, 0, 0x00 ; halt r2 
 nano_code_len = $ - nano_code
