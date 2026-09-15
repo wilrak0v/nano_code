cfd16041d23591020aab24ef7d2bac25f98448bb
wilrak0v <wilrakov@proton.me>
2026-09-15 19:49:41 +0200

    feat(customasm): fix the movi instruction

--- customasm/nanocode.asm
+++ customasm/nanocode.asm
@@ -6,8 +6,6 @@
 }
 
 #ruledef main {
-    halt {r: reg} => 0x00 @ r @ 0x00000
-    movi {r: reg}, {im: u16} => 0x01 @ r @ im
-    movl {r: reg}, {im: u32} => 0x02000000 @ r
-    movr {dst: reg}, {src: reg} => 0x03 @ dst @ src @ 0x00
+    halt {r: reg} => 0x00 @ r @ r @ r 
+    movi {r: reg}, {im: u16} => 0x01 @ r @ im[7:0] @ im[15:8] 
 }
