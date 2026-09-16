e81b858f48871012b36d035be02cddff567c2a51
wilrak0v <wilrakov@proton.me>
2026-09-16 16:04:41 +0200

    feat(customasm): simple fibonacci calcul

--- customasm/nanocode.asm
+++ customasm/nanocode.asm
@@ -3,9 +3,18 @@
     r1 => 0x01
     r2 => 0x02
     r3 => 0x03
+    r4 => 0x04
 }
 
 #ruledef main {
     halt {r: reg} => 0x00 @ r @ r @ r 
     movi {r: reg}, {im: u16} => 0x01 @ r @ im[7:0] @ im[15:8] 
+    movl {r: reg}, {im: u32} => 0x02 @ r @ 0x0000 @ im[7:0] @ im[15:8] @ im[23:16] @ im[31:24]
+    movr {dst: reg}, {src: reg}  => 0x03 @ dst @ src @ 0x00
+    add {dst: reg}, {src: reg} => 0x0A @ dst @ src @ 0x00
+    subi {dst: reg}, {im: u16} => 0x10 @ dst @ im[7:0] @ im[15:8]
+    cmpi {r: reg}, {im: u16}    => 0x25 @ r @ im[7:0] @ im[15:8] 
+    jei [{address: u16}]        => 0x26 @ address[7:0] @ address[15:8] @ 0x00
+    jnzi {r: reg}, [{address: u16}] => 0x2a @ r @ address[7:0] @ address[15:8]
+    jmpi [{address: u16}]       => 0x2b @ address[7:0] @ address[15:8] @ 0x00
 }
--- customasm/test.asm
+++ customasm/test.asm
@@ -1,5 +1,22 @@
 #include "nanocode.asm"
 
 start:
-    movi r1, 17
-    halt r1
+    movl r0, 10000 
+    movi r1, 0
+    movi r2, 1
+
+loop:
+    cmpi r0, 1
+    jei [done]
+
+    movr r3, r1
+    add r3, r2
+
+    movr r1, r2
+    movr r2, r3
+
+    subi r0, 1
+    jmpi [loop]
+
+done:
+    halt r2
